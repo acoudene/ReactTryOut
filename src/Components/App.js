317330fe13cf0fefc22538410fa9600d8acfbedb
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-05-18 19:48:27 +0200

    Add questions

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,9 @@ import Banner from './Banner'
 import logo from '../Assets/logo.png'
 // import Cart from './Cart'
 import ShoppingList from './ShoppingList'
+import QuestionFormU from './QuestionFormU';
+import QuestionForm from './QuestionForm';
+
 
 // Regarder CSS-IN-JS
 
@@ -12,6 +15,8 @@ function App() {
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
+			<QuestionFormU />
+			<QuestionForm />
 			{/* <Cart /> */}
 			<ShoppingList />
 		</div>
